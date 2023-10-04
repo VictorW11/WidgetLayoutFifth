d724657a0ea1aef0455aab3d34fe938b5b7631de
VictorW11 <wattenbargerv@gmail.com>
2023-10-04 13:17:24 -0500

    Added BoxLayout and StackLayout

--- main.py
+++ main.py
@@ -5,19 +5,24 @@ from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 
 
-class StackLayoutExample(StackLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.orientation = "lr-tb"
-        b = Button(text="A", size_hint=(.5,.5))
 
-class BoxLayoutExample(BoxLayout):
-    pass
+# class StackLayoutExample(StackLayout):
+   # def __init__(self, **kwargs):
+       # super().__init__(**kwargs)
+       # self.orientation = "lr-tb"
+       # b = Button(text="A", size_hint=(.5,.5))
 
-class MainWidget(Widget):
-    pass
+
+# class BoxLayoutExample(BoxLayout):
+   # pass
+
+# class MainWidget(Widget):
+   # pass
 
 class WidgetLayoutApp(App):
+    pass
+
+class LayoutAssignment(BoxLayout):
     pass
 
 WidgetLayoutApp().run()
